dc989243f2579995597c96eb540017c29b57644a
Dave Matthews <davemattcsu@gmail.com>
2022-09-06 20:11:18 -0600

    Initial team id and about information.

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -3,17 +3,17 @@ import teamPic from "../../static/images/PlaceholderTeam.jpg";
 
 export const teamData =
     {
-        teamName: "Team Name",
-        missionStatement: "Mission Statement",
+        teamName: "t00 dles",
+        missionStatement: "CS 314 Software Engineering introduces students to team-based development of large, complex software systems. Major topics include Agile software development, clean code, configuration management, continuous integration, project management, requirements management, teamwork, and test driven development. Students learn integrated tools to support a Scrum-based development process while completing a semester-long web development project. This course prepares students to work in teams in upper level courses, industry, research, or the open-source community.",
         imagePath: teamPic,
     };
 
 
 export const memberData = [
     {
-        name: "Team Member 1",
-        bio: "Bio Here",
-        homeTown: "Hometown",
+        name: "Dave Matthews",
+        bio: "I teach undergraduate courses in the Computer Science Department at Colorado State University while pursing a Ph.D. in Computer Science with research interests in Optimization, Machine Learning, and Education. ",
+        homeTown: "Coeur d'Alene, Idaho",
         imagePath: memberPic
     },
     {
