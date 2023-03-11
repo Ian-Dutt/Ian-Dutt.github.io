2a0800d42a1352ca20dfb1a21d6fe28ec46c894c
Ian-Dutt <ianadutt@colostate.edu>
2023-03-10 18:55:13 -0700

    template

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./styles/App.scss";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
-function App() {
+function App(props) {
+  return (
+    <>
+      <Header />
+      <MainBody />
+      <Footer />
+    </>
+  );
+}
+
+function MainBody() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Tis a bit empty, but dont fret. Ill finish this sometime this brek.</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a
+            className="App-link"
+            href="https://github.com/ian-dutt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          {/* &bull; */}
+          {/* <a
+            className="App-link"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a> */}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
